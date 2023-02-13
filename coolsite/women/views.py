e065cddcf8998d29d0d5bbc4097dbb45d151047b
Teonanakatll <105820090+Teonanakatll@users.noreply.github.com>
2023-02-13 10:18:43 +0300

    Отлов GET и POST запросов

--- coolsite/women/views.py
+++ coolsite/women/views.py
@@ -5,6 +5,8 @@ def index(request):
     return HttpResponse("Страница приложения women.")
 
 def categories(request, catid):
+    if request.GET:
+        print(request.GET)
     return HttpResponse(f"<h1>Статьи по категориям</h1><p>{catid}</p>")
 
 def archive(request, year):
