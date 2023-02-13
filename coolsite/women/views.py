35fc02cbf644ff4e93e4113a3da118ef6dd0c199
Teonanakatll <105820090+Teonanakatll@users.noreply.github.com>
2023-02-13 23:06:26 +0300

    #6___________ ШАБЛОНЫ - НАЧАЛО, cоздание шаблонов index.html и about.html расширяя базовый шаблон, передача данных в шаблоны через параметры в функциях представлений

--- coolsite/women/views.py
+++ coolsite/women/views.py
@@ -1,11 +1,13 @@
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import render, redirect
 
+menu = ["О сайте", "Добавить статью", "Обратная связь", "Войти"]
+
 def index(request):
-    return render(request, 'women/index.html')
+    return render(request, 'women/index.html', {'menu': menu, 'title': 'Главная страница'})
 
 def about(request):
-    return render(request, 'women/about.html')
+    return render(request, 'women/about.html', {'menu': menu, 'title': 'О сайте'})
 
 def categories(request, catid):
     if request.GET:
