3525b95891261fea8ca9a7eb556e25576880acc7
Teonanakatll <105820090+Teonanakatll@users.noreply.github.com>
2023-02-13 11:00:36 +0300

    cоздание 301 и 302 redirect, cоздание имён для url адресов

--- coolsite/coolsite/urls.py
+++ coolsite/coolsite/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from урок7.coolsite.women.views import pageNotFound
+
+from women.views import pageNotFound
 
 urlpatterns = [
     path('admin/', admin.site.urls),
