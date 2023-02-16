433e34398200fdc9c2db8e99fa22790b413aa7b0
Teonanakatll <105820090+Teonanakatll@users.noreply.github.com>
2023-02-16 11:02:04 +0300

    #10_________РАБОТА С АДМИН ПАНЕЛЬЮ

--- coolsite/women/admin.py
+++ coolsite/women/admin.py
@@ -1,5 +1,27 @@
 from django.contrib import admin
 
-from women.models import Women
+from women.models import Women, Category
 
-admin.site.register(Women)
+class WomenAdmin(admin.ModelAdmin):
+    # Поля которые будут отображаться в админ панели
+    list_display = ('id', 'title', 'time_create', 'photo', 'is_published')
+
+    # Поля на которые мы можем кликнуть и перейти
+    list_display_links = ('id', 'title')
+
+    # По каким полям можно будет производить поиск
+    search_fields = ('title', 'content')
+
+    # Список полей которые можно редактировать в админ панели
+    list_editable = ('is_published',)
+
+    # Поля по которым можно фильтровать список статей (сайт-бар)
+    list_filter = ('is_published', 'time_create')
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    list_display_links = ('id', 'name')
+    search_fields = ('name',)
+
+admin.site.register(Women, WomenAdmin)
+admin.site.register(Category, CategoryAdmin)
