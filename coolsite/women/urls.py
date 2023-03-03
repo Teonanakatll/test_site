88a3ec737b120cfd88b053ac2cbc443957bbc355
Teonanakatll <105820090+Teonanakatll@users.noreply.github.com>
2023-03-03 11:31:23 +0300

    кеширование на уровне шаблонов

--- coolsite/women/urls.py
+++ coolsite/women/urls.py
@@ -5,7 +5,7 @@ from .views import archive, about, contact, WomenHome, WomenCategory, ShowPost, 
     logout_user
 
 urlpatterns = [
-    path('', cache_page(60)(WomenHome.as_view()), name='home'),
+    path('', WomenHome.as_view(), name='home'),
     path('about/', about, name='about'),
     path('addpage/', AddPage.as_view(), name='add_page'),
     path('contact/', contact, name='contact'),
@@ -13,7 +13,7 @@ urlpatterns = [
     path('logout/', logout_user, name='logout'),
     path('register/', RegisterUser.as_view(), name='register'),
     path('post/<slug:post_slug>/', ShowPost.as_view(), name='post'),
-    path('category/<slug:cat_slug>/', cache_page(60)(WomenCategory.as_view()), name='category'),
+    path('category/<slug:cat_slug>/', WomenCategory.as_view(), name='category'),
 
     # path('cats/<int:catid>/', categories),
     re_path(r'^archive/(?P<year>[0-9]{4})/', archive),
